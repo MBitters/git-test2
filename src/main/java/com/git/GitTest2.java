da5caacfadb3d43cbd295ac4ede9a212b8a93488
jjr <1925373981@qq.com>
2022-03-23 02:34:02 +0800

    hot-fix 第yi次提交

--- src/main/java/com/git/GitTest2.java
+++ src/main/java/com/git/GitTest2.java
@@ -4,5 +4,6 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("111");
         System.out.println("2222");
+        System.out.println("hot-fix");
     }
 }
